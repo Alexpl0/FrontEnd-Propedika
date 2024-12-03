fa7445f8cdc9fd86004abf89b02af44738fd405e
Alexpl0 <j.alejandro.pl@gmail.com>
2024-12-03 13:14:11 -0600

    DELETE ok

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ const email = document.getElementById('email');
 const edad = document.getElementById('edad');
 const telefono = document.getElementById('telefono');
 const sexo = document.getElementById('sexo');
+const tablaAlumnos = document.querySelector('.table');
+const formAlumno = document.querySelector('.tablaAlumno');
 
-const dataTableOptions = {
+const dataTableOptions ={
     //scrollX: "2000px",
     destroy: true,
-    select: true,
+    select: false,
 };
 
 const initDataTable = async () => {
@@ -36,20 +38,23 @@ const listAlumnos= async () => {
         alumno.forEach((alumno, index) => {
             content+=`
             <tr>
+            <div>
                 <td><input type="checkbox" id="checkbox"></td>
-                <td>${alumno.id}</td>
-                <td>${alumno.nombre}</td>
-                <td>${alumno.apellido}</td>
-                <td>${alumno.email}</td>
-                <td>${alumno.edad}</td>
-                <td>${alumno.telefono}</td>
-                <td>${alumno.sexo}</td>
+                <td id="alumnoID">${alumno.id}</td>
+                <td id="nombre">${alumno.nombre}</td>
+                <td id="apellido">${alumno.apellido}</td>
+                <td id="email">${alumno.email}</td>
+                <td id="edad">${alumno.edad}</td>
+                <td id="telefono">${alumno.telefono}</td>
+                <td id="sexo">${alumno.sexo}</td>
+                <div>
                 <td>
-                        <button class="btn btn-sm btn-primary"><i class="fa-solid fa-pencil"></i></button>
-                        <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash"></i></button>
+                    <button class="btn btn-sm btn-primary" id="editar" class="editar"><i class="fa-solid fa-pencil"></i></button>
+                    <button class="btn btn-sm btn-danger" id="borrar" class="borrar"><i class="fa-solid fa-trash"></i></button>
                 </td>
-
-                </tr>`;
+                </div>
+            </div>
+            </tr>`;
         });
         bodyAlumno.innerHTML = content;
     }
@@ -106,3 +111,78 @@ addAlumno.addEventListener('submit', ( e ) => {
 });
 
 
+formAlumno.addEventListener('click', (e) => {
+    //console.log(e.target.id);
+    e.preventDefault();
+
+    let deleteButton = e.target.id=='borrar';
+    let editButton = e.target.id=='editar';
+    
+    let id= e.target.parentElement.parentElement.querySelector('#alumnoID').textContent;
+
+    if(deleteButton){
+        console.log('borrar alumno');
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: 'No podrás revertir esto!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, borrarlo!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`${web}/${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8'
+                    }
+                    
+                })
+                
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(`${web}/${id}`);
+                    Swal.fire(
+                        'Borrado!',
+                        'El alumno ha sido borrado.',
+                        'success'
+                    ).then(() => {
+                        window.location.reload();
+                    });
+                })
+                .catch((error) => {
+                    console.error('There was a problem with the fetch operation:', error);
+                });
+                window.location.reload();
+            }
+        });
+    }
+
+    if(editButton){
+        let parent = e.target.parentElement.parentElement;
+        console.log('editar alumno');
+        console.log(parent);
+        let idE = parent.querySelector('#alumnoID').textContent;
+        let nombreE = parent.querySelector('#nombre').textContent;
+        let apellidoE = parent.querySelector('#apellido').textContent;
+        let emailE = parent.querySelector('#email').textContent;
+        let edadE = parent.querySelector('#edad').textContent;
+        let telefonoE = parent.querySelector('#telefono').textContent;
+        let sexoE = parent.querySelector('#sexo').textContent;
+        console.log(idE, nombreE, apellidoE, emailE, edadE, telefonoE, sexoE);
+        
+        nombre.value = nombreE;
+        apellido.value = apellidoE;
+        email.value = emailE;
+        edad.value = edadE;
+        telefono.value = telefonoE;
+        sexo.value = sexoE;
+        
+    }
+});
